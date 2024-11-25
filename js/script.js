a6ad4582317dfee5c21e0018aac8174406508b35
DarynaPankovetska <you@example.comdarynapankovetska@gmail.com>
2024-11-25 17:26:00 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+new WOW().init();
+
+let menuButton = document.querySelector('.menu_button');
+let nav = document.querySelector('.nav');
+
+menuButton.addEventListener('click', function () {
+	nav.classList.toggle('active');
+});
